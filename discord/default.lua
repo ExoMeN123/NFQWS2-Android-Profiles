cc2876a8d426a0e370d314d087fc03facd6314cd
ExoMeN123 <94221619+ExoMeN123@users.noreply.github.com>
2026-08-12 19:48:31 +0300

    Add Discord strategy placeholder

---
+++ discord/default.lua
@@ -0,0 +1,9 @@
+-- NFQWS2 Android Strategy Pack: Discord
+-- Reserved for the upcoming nfqws2/Lua bridge.
+-- Functional Preview 0.4 does not execute this file yet.
+
+return {
+  service = "discord",
+  version = 1,
+  status = "reserved"
+}
